79ebdfc33274cb4615d760bf72732ae332c4ccee
dzfacemaker-ops <dzfacemaker@gmail.com>
2026-06-09 17:22:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,237 @@
+const database = {
+    "Premier League": {
+        "Chelsea": [
+            
+        ],
+        "Manchester United": [], "Liverpool": [], 
+        "Manchester City": [
+            { name: "Reigan Heskey", img: "Images/Screenshot 2026-04-11 221356.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/ToA4qX " },  
+        ], 
+        "Arsenal": [
+            { name: "Max Dowman", img: "Images/Screenshot 2026-04-10 222808.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/hIREWxf " },  
+        ],
+        "Tottenham Hotspur": [
+            { name: "Xavi Simons", img: "Images/Screenshot 2026-05-13 224822.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/bhcDOy1 " },   
+        ], 
+    },
+    "Championship (ENG 2)": { "Leicester City": [], "Leeds United": [], "Southampton": [] },
+    "La Liga": {
+        "Barcelona": [
+            { name: "Xavi Espart", img: "Images/Screenshot 2026-04-09 052224.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/5m5tzV " },
+            { name: "Lamine Yamal", img: "Images/Screenshot 2026-05-03 230458.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/H8TKMu " },
+            { name: "Raphinha", img: "Images/Screenshot 2026-05-09 000349.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/O6PYCI " }
+        ],
+        "Real Madrid": [
+            { name: "Kelyan Mbappè", img: "Images/Screenshot 2026-05-15 010107.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/w5h6bN " },
+            { name: "Jude Bellingham", img: "Images/Screenshot 2026-05-17 210335.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/2Sj46D " },
+            { name: "Thiago Pitarch", img: "Images/Screenshot 2026-04-10 202531.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/kz52PWz" },
+            { name: "Diego Aguado", img: "Images/Screenshot 2026-04-30 182143.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/Ey2Bew " },
+            { name: "Alvaro Leiva", img: "Images/Screenshot 2026-05-01 194953.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/ZVa4yKN " },
+            { name: "Thibaut courtois", img: "Images/Screenshot 2026-05-25 220104.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/TUN9Uw " }
+        
+        ],
+     
+         "Atletico Madrid": [
+           { name: "Juan MUsso", img: "Images/Screenshot 2026-05-26 202631.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/m2q40y " }
+         ]
+    },
+    "La Liga 2": { "Espanyol": [], "Real Zaragoza": [] },
+    "Serie A": { "Juventus": [], "Inter Milan": [], 
+        "AC Milan": [
+            { name: "Loftus Cheek", img: "Images/Screenshot 2026-04-12 224508.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/wqHjzW " },
+            { name: "Strahinja Pavlovic", img: "Images/Screenshot 2026-05-25 003414.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/IwYb0VT " },
+        ] },
+    "Serie B": { "Sampdoria": [], "Bari": [], 
+        "Helas Virona": [
+            { name: "Rafik Belghali", img: "Images/Screenshot 2026-04-08 021035.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/GGXJPdK " }
+        ] },
+    "Bundesliga": { "Bayern Munich": [
+            { name: "Michael Olice", img: "Images/Screenshot 2026-05-07 002206.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/CSIt1Y " },
+            { name: "Luis Diaz", img: "Images/Screenshot 2026-05-27 225346.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/m04yQZ " }
+    ], "Borussia Dortmund": [], "Bayer Leverkusen": [] },
+    "Bundesliga 2": { "Schalke 04": [], "Hertha BSC": [] },
+    "Ligue 1": { "PSG": [
+            { name: "Vitinha", img: "Images/Screenshot 2026-05-16 235613.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/kW2Y7L0 " },
+            { name: "Désiré Doué", img: "Images/Screenshot 2026-04-27 170905.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/G0OxzC" },
+            { name: "Dro Fernàndez", img: "Images/Screenshot 2026-05-21 174814.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/JQqs8X " }
+    ], "Olympique Marseille": [], "Monaco": [] },
+    "Ligue 2": { "Bordeaux": [], "Saint-Etienne": [] },
+    "Algerian Ligue 1": {
+        "MC Alger": [
+            { name: "Youcef Belaili", img: "Images/Screenshot 2026-04-14 225327.png", details: "Exclusive Face Pack for Algerian Ligue 1 project in PES 17.", link: " https://ouo.io/52BCsW " }
+        ],
+        "CR Belouizdad": [], "JS Kabylie": [], "USM Alger": [], "ES Setif": [], "CS Constantine": []
+    },
+    "Other": {
+        "Classic": [
+            { name: "John Terry", img: "Images/Screenshot 2026-05-05 055549.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/Y4FxIh " },
+            { name: "Hugo Sànchez", img: "Images/Hugo Sanchez.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: " https://ouo.io/zjSDzM" },
+            { name: "Cicinho", img: "Images/Screenshot 2026-04-23 182915.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/29kI7p " },
+            { name: "Tarek El Taib", img: "Images/Screenshot 2026-04-24 185915.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/NfDEkR" },
+            { name: "Tarek El Taib V2", img: "Images/Screenshot 2026-05-09 233706.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/q0FrmH" },
+            { name: "Abbondanzieri", img: "Images/Screenshot 2026-04-25 221355.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/i0w8Y5" },
+            { name: "John Terry V2", img: "Images/Screenshot 2026-05-05 055549.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/5ZGe3dU" },
+            { name: "Kakà", img: "Images/Screenshot 2026-05-11 232034.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/PbUVef " },
+            { name: "Jon Dahl Tomasson", img: "Images/Screenshot 2026-05-19 222106.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/Y5vxpl" },
+            { name: "Woodgate", img: "Images/Screenshot 2026-04-21 040902.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/RQIzGIA" },
+
+        ],
+        "Other clubs": [
+            { name: "Neymar Jr", img: "Images/Screenshot 2026-06-08 213304.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/Yyx5jg7" },
+            { name: "Adil Boulbina", img: "Images/Screenshot 2026-04-14 015340.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/aPemCo " },
+            { name: "Omar Ben Ali", img: "Images/Screenshot 2026-04-19 183718.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/z5TdEVX" },
+            { name: "Assad Al Hamlaoui", img: "Images/Screenshot 2026-04-22 204516.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/0U5zEdA" },
+            { name: "Javier Hernàndez", img: "Images/Screenshot 2026-05-08 034824.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/aXWztm7" },
+            { name: "Ugochukwu Iwo", img: "Images/Screenshot 2026-05-19 004132.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/YD6BjZ " },
+            { name: "Zineddine Belaid", img: "Images/Screenshot 2026-04-15 211742.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/Oea2hT" },
+            { name: "Nadhir Benbouali", img: "Images/Screenshot 2026-05-02 043748.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/l42rLVj" },
+            { name: "Matteo Palma", img: "Images/Screenshot 2026-05-06 045121.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/1tyyVv " },
+            { name: "Rafik Messali", img: "Images/Screenshot 2026-05-02 230700.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/k6K21O" },
+            { name: "Hannial Mejbri", img: "Images/Screenshot 2026-04-29 220828.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/7q7Ea9n " },
+            { name: "Ali Youssef", img: "Images/Screenshot 2026-04-28 045506.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/jJNwdK" },
+            { name: "Suhaib Nair", img: "Images/Screenshot 2026-04-24 225911.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/aroCIzo" },
+            { name: "Elies Araar", img: "Images/araar.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/X0ZAlf" },
+            { name: "Eduard Spertsyan", img: "Images/Screenshot 2026-05-21 002738.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/RXM8KX " },
+            { name: "Erik Piloyan", img: "Images/Screenshot 2026-05-23 230457.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/RQJhSSZ" },
+            { name: "Relebohile Mofokeng", img: "Images/Screenshot 2026-05-15 194033.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/FQ77Zr " },
+            { name: "Zakaria Eddahchouri", img: "Images/Screenshot 2026-05-22 225029.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/gGYUnj " },
+            { name: "Antoine Griezmann", img: "Images/Screenshot 2026-05-10 235423.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/JIP28z" },
+            { name: "Mohamed Amoura", img: "Images/Screenshot 2026-04-15 181002.png", details: "Premium high quality ultra-textured face pack for PES 17.", link: "https://ouo.io/46bbgd" },
+           
+
+
+
+            
+            
+        ]
+     },          
+
+    
+
+
+};
+
+const leaguesList = document.getElementById('leagues-list');
+const displayGrid = document.getElementById('display-grid');
+const playerModal = document.getElementById('player-modal');
+const breadcrumb = document.getElementById('breadcrumb');
+const searchInput = document.getElementById('search-input');
+const backToHomeBtn = document.getElementById('back-to-home');
+
+function init() {
+    if(leaguesList) {
+        leaguesList.innerHTML = '';
+        Object.keys(database).forEach(league => {
+            const li = document.createElement('li');
+            li.innerHTML = `<i class="far fa-circle-dot" style="margin-right:8px; font-size:0.8rem;"></i> ${league}`;
+            li.onclick = () => showClubs(league);
+            leaguesList.appendChild(li);
+        });
+    }
+    showAllLatestFaces();
+    
+    document.getElementById('close-modal-btn').onclick = () => playerModal.style.display = 'none';
+    backToHomeBtn.onclick = showAllLatestFaces;
+}
+
+function createPlayerCard(player, clubName) {
+    const card = document.createElement('div');
+    card.className = 'face-card';
+    card.innerHTML = `
+        <div class="card-img-holder">
+            <img src="${player.img}" alt="${player.name}" onerror="this.src='https://via.placeholder.com/220x200?text=No+Preview'">
+        </div>
+        <div class="card-details">
+            <div>
+                <h3>${player.name}</h3>
+                <p class="card-club"><i class="fas fa-shield-halved"></i> ${clubName}</p>
+            </div>
+            <button class="card-btn">View Details</button>
+        </div>
+    `;
+    card.querySelector('.card-btn').onclick = () => openPlayerModal(player, clubName);
+    return card;
+}
+
+function showAllLatestFaces() {
+    displayGrid.innerHTML = '';
+    breadcrumb.textContent = "Latest Premium Faces";
+    backToHomeBtn.style.display = 'none';
+    searchInput.value = '';
+
+    let hasPlayers = false;
+    for (const league in database) {
+        for (const club in database[league]) {
+            database[league][club].forEach(player => {
+                hasPlayers = true;
+                displayGrid.appendChild(createPlayerCard(player, club));
+            });
+        }
+    }
+    if(!hasPlayers) displayGrid.innerHTML = '<p style="color:#888; text-align:center; width:100%;">No faces uploaded yet.</p>';
+}
+
+function showClubs(leagueName) {
+    displayGrid.innerHTML = '';
+    breadcrumb.textContent = leagueName;
+    backToHomeBtn.style.display = 'inline-block';
+    searchInput.value = '';
+
+    const clubs = Object.keys(database[leagueName]);
+    clubs.forEach(club => {
+        const div = document.createElement('div');
+        div.className = 'grid-item';
+        div.innerHTML = `<i class="fas fa-shield-halved" style="color:#00e676; display:block; font-size:1.5rem; margin-bottom:10px;"></i> ${club}`;
+        div.onclick = () => showPlayers(leagueName, club);
+        displayGrid.appendChild(div);
+    });
+}
+
+function showPlayers(leagueName, clubName) {
+    displayGrid.innerHTML = '';
+    breadcrumb.textContent = `${leagueName} > ${clubName}`;
+
+    const players = database[leagueName][clubName];
+    if(players.length === 0) {
+        displayGrid.innerHTML = '<p style="color:#888; text-align:center; width:100%;">No faces available for this club.</p>';
+        return;
+    }
+    players.forEach(player => {
+        displayGrid.appendChild(createPlayerCard(player, clubName));
+    });
+}
+
+function openPlayerModal(player, clubName) {
+    document.getElementById('player-img').src = player.img;
+    document.getElementById('player-name').textContent = player.name;
+    document.getElementById('player-club-tag').querySelector('span').textContent = clubName;
+    document.getElementById('player-info').textContent = player.details;
+    document.getElementById('download-link').href = player.link;
+    playerModal.style.display = 'flex';
+}
+
+if(searchInput) {
+    searchInput.addEventListener('input', (e) => {
+        const query = e.target.value.toLowerCase().trim();
+        if(query === '') { showAllLatestFaces(); return; }
+
+        displayGrid.innerHTML = '';
+        breadcrumb.textContent = `Search results for: "${query}"`;
+        backToHomeBtn.style.display = 'inline-block';
+
+        let found = false;
+        for (const league in database) {
+            for (const club in database[league]) {
+                database[league][club].forEach(player => {
+                    if(player.name.toLowerCase().includes(query)) {
+                        found = true;
+                        displayGrid.appendChild(createPlayerCard(player, club));
+                    }
+                });
+            }
+        }
+        if(!found) displayGrid.innerHTML = '<p style="color:#aaafab; text-align:center; width:100%; margin-top:20px;">No matching players found.</p>';
+    });
+}
+
+window.onload = init;
